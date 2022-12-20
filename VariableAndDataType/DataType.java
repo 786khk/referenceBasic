00af9a1051ed7b8da018303847bde3179e4f2d18
786khk <78067072+786khk@users.noreply.github.com>
2022-12-20 22:58:15 +0900

    변수 정리 완료 졸립다

---
+++ VariableAndDataType/DataType.java
@@ -0,0 +1,7 @@
+package VariableAndDataType;
+
+public class DataType {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ VariableAndDataType/VariableNaming.java
@@ -0,0 +1,10 @@
+package VariableAndDataType;
+
+public class VariableNaming{    
+    public static void main(String[] args) {
+        String userName = "홍길동";
+        String city1 = "서울";
+        boolean tryEatDinner = true;
+        double JAVA_VERSION = 8.0; 
+    }
+}
