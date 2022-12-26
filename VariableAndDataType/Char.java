b25d14aac77899a035a5c4c341a5914e9a7591e0
786khk <78067072+786khk@users.noreply.github.com>
2022-12-26 20:32:38 +0900

    문자형

---
+++ VariableAndDataType/Char.java
@@ -0,0 +1,10 @@
+package VariableAndDataType;
+
+public class Char{
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = '\u0041';
+        System.out.println("c1 : " + c1);
+        System.out.println("c2 : " + c2);        
+    }
+}
