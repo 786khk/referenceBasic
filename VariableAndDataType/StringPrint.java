2c3859a19695fd63b3a52fd810f2b3b63681853c
786khk <78067072+786khk@users.noreply.github.com>
2022-12-26 21:21:58 +0900

    String

---
+++ VariableAndDataType/StringPrint.java
@@ -0,0 +1,13 @@
+package VariableAndDataType;
+
+public class String {
+    public static void main(String[] args) {
+        String s1 = "string과 char는 다르다.";
+        String s2 = "안녕, \tJAVA의 세계에 온것을 \t환영해";
+        System.out.println(s1);
+        System.out.println(s2);
+
+        
+        
+    }
+}
