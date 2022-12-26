ed81946d0daa15f0f255a7daaf85317f308d7d6a
786khk <78067072+786khk@users.noreply.github.com>
2022-12-26 21:06:56 +0900

    Boolean.java

---
+++ VariableAndDataType/Boolean.java
@@ -0,0 +1,14 @@
+package VariableAndDataType;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        if(b1){
+            System.out.println(b1 + "입니다!!");
+        }else{
+
+            System.out.println(b2 + "입니다..");
+        }
+    }
+}
