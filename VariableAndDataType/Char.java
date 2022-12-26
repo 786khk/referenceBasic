8f3dcd20725fbbe424773b562655921e1752f781
786khk <78067072+786khk@users.noreply.github.com>
2022-12-26 20:55:41 +0900

    Char.java

--- VariableAndDataType/Char.java
+++ VariableAndDataType/Char.java
@@ -5,6 +5,17 @@ public class Char{
         char c1 = 'A';
         char c2 = '\u0041';
         System.out.println("c1 : " + c1);
-        System.out.println("c2 : " + c2);        
+        System.out.println("c2 : " + c2); 
+        
+        int i = 'A' + 1; // A == 65
+        System.out.println(i);
+
+        char intToChar = (char)i;
+        System.out.println(intToChar);
+
+        int i2 = '가'+1;
+        System.out.println(i2);
+
+        // int i3 = "가지" + 1; //char형은 유니코드를 표현할 수 있다. char형의 리터럴은 '', ""은 문자열  
     }
 }
