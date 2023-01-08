3aa1e1b81f52c5f89116b607474a753595acbe9a
786khk <78067072+786khk@users.noreply.github.com>
2023-01-08 22:16:57 +0900

    논리연산, 증감연산,삼항연산

---
+++ operation/TrinomialOperation.java
@@ -0,0 +1,10 @@
+package operation;
+
+public class TrinomialOperation {
+    public static void main(String[] args) {
+        String intType = (10%2)>0?"홀수":"짝수";
+        System.out.println(intType);
+    
+    }
+    
+}
---
+++ operation/TypeCastingIncrementalOperation.java
@@ -0,0 +1,15 @@
+package operation;
+
+public class TypeCastingIncrementalOperation {
+    public static void main(String[] args) {
+        byte b1=10;
+        b1 += 1; // 복합연산, 별도의 형변환 없으므로 대입가능
+        System.out.println(b1);
+
+        byte b2 = ++b1; //b1을 1증가해 할당, 별도 형변환 없음
+        System.out.println(b2);
+        // byte b3 = (b2 + 1); //Type mismatch: cannot convert from int to byte
+        byte b3 = (byte)(b2+1); //b2의 값이 자동으로 int로 형변환하므로 (byte)라는 명시적 형변환이 필요
+        System.out.println(b3);
+    }
+}
