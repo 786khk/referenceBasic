0c5b5a344513b298bf305a53062703d1f60e8845
hyunkyungkim <786khk@injeinc.co.kr>
2023-02-27 20:08:18 +0900

    커스텀 에러 예제

--- Exception/NewExceptionTest.java
+++ Exception/NewExceptionTest.java
@@ -3,10 +3,37 @@ package Exception;
 public class NewExceptionTest {
     public static void main(String[] args) {
         try {
-            
-        } catch (Spa e) {
+            startInstall();
+            coptFiles();
+        } catch (SpaceException se) {
+            System.out.println("에러"+se.getMessage());
+            se.printStackTrace();
+            System.out.println("설치할 공간 확보요망");
             // TODO: handle exception
-        } catch (Exception e) {
-        }
+        } catch (MemoryException me) {
+            System.out.println("에러"+me.getMessage());
+            me.printStackTrace();
+            System.out.println("다시 설치 시도요망");
+        }finally{
+            deleteTempFiles();
+        }  
     }
+
+    static void startInstall() throws SpaceException, MemoryException{
+        if(!enoughSpace()) throw new SpaceException("설치공간 부족");
+        if(!enoughMemory()) throw new MemoryException("메모리 부족");
+
+    }
+    static void coptFiles(){System.out.println("설치파일 복사!");};
+    static void deleteTempFiles(){System.out.println("설치파일 삭제!");};
+    static boolean enoughSpace(){return false;}
+
+    static boolean enoughMemory(){ return true;}
+    
+}
+class SpaceException extends Exception{
+    SpaceException(String msg){super(msg);}
+}
+class MemoryException extends Exception{
+    MemoryException(String msg){super(msg);  }
 }
