64aac22d094ff6ba111b0fe37a68e156292cd7c2
786khk <786khk@injeinc.co.kr>
2023-05-23 14:19:36 +0900

    비트연산자

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import Operator.OperatorEx21;
 import Operator.OperatorEx22;
 import Operator.OperatorEx23;
+import Operator.OperatorEx28;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +9,11 @@ public class Main {
         // OperatorEx21 o21 = new OperatorEx21();
         // o21.main();
 
-        OperatorEx23 o22 = new OperatorEx23();
-        o22.main();
+        // OperatorEx23 o22 = new OperatorEx23();
+        // o22.main();
+        OperatorEx28 o28 = new OperatorEx28();
+
+        o28.main();
 
     }
 }
--- Operator/Main.java
+++
@@ -1,7 +0,0 @@
-package Operator;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
